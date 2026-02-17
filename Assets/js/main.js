669e2ca6884b865ccf953e42c10cee12ec0f7401
Owen Howarth <141244556+Itsorh@users.noreply.github.com>
2026-02-17 12:16:36 +0000

    Delete Assets directory
    Deleted uppercase 'assets' folder to replace with lower case to maintain consistencies.

--- Assets/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const burger = document.querySelector('.menu-toggle');
-  const navList = document.querySelector('#topnav ul');
-
-  burger.addEventListener('click', () => {
-    navList.classList.toggle('show');
-  });
-});
-
